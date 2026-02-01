aa795391c00bb9361e423b2ea83349ee97d4853a
ajaysth <shrestha11ajay@gmail.com>
2026-02-01 09:33:38 +0545

    ProductDetail page created

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ProductDetail from "./pages/ProductDetail"
+
+
+function App() {
+
+  return (
+    <>
+      <ProductDetail />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/ProductDetail.tsx
@@ -0,0 +1,14 @@
+
+const ProductDetail = () => {
+    return (
+        <>
+            <div className="p-4">
+                {/* breadcrumbs */}
+                <div>Football / Shoes / Nike</div>
+
+            </div>
+        </>
+    )
+}
+
+export default ProductDetail
